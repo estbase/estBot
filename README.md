26e2feddf0867522c3f07154b7859bd258bd983e
Oriol Navascuez <onavascuez@gmail.com>
2019-01-22 23:35:08 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# EST Discord Bot
+
+## Objectives
+The objective about this bot, in first place is
+
+## How to Install
+ASAP
+
+## Commands
+List of actual commands:
+
+* **!clear [i]** : On [i] is an integer with number of messages to delete.
+* **!help** : Command to view what commands is available
+* **!ip** : Sends a private message with all info aboit your IP, City, CPA,...
+* **!ping** : Send private messages with your private and public IP
+* **!userinfo** : Send channel message with your basic user info on Discord.
+
+## TODO
+* [ ] Check access to some commands
+* [ ] Control num of servers to connect to our API with TOKEN
---
+++ main.py
@@ -0,0 +1,51 @@
+import asyncio
+import discord
+from discord import Game, Embed, Color
+
+from commands import cmd_ping, cmd_ip, cmd_clear, cmd_userinfo, cmd_help
+
+TOKEN = 'NTM2ODY3ODc3NzAyMjA1NDUw.DydTkg.frUnL693WrackjaMPV0yKqTjWFQ'
+BOT_PREFIX = ('!', '-')
+
+client = discord.Client()
+
+
+commands = {
+    "ping": cmd_ping,
+    "ip": cmd_ip,
+    "clear": cmd_clear,
+    'userinfo': cmd_userinfo,
+    'help': cmd_help,
+}
+
+# When BOT is ready
+@client.event
+@asyncio.coroutine
+def on_ready():
+    try:
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
+        print('Bot is logged in successfully. Running on servers: \n')
+        for s in client.servers:
+            print(" - %s (%s)" % (s.name, s.id))
+        yield from client.change_presence(game=Game(name="Testing BOT"))
+    except Exception as e:
+        print(e)
+
+# When receive a new message
+@client.event
+@asyncio.coroutine
+def on_message(message):
+    if message.content.startswith(BOT_PREFIX):
+        invoke = message.content[1:].split(" ")[0]
+        args = message.content.split(" ")[1:]
+        print("INVOKE: %s\nARGS: %s" % (invoke, args.__str__()))
+        if commands.__contains__(invoke):
+            yield from commands.get(invoke).handle(args, message, client, invoke)
+        else:
+            yield from client.send_message(message.channel, embed=Embed(color=Color.red(), description=("The command '%s' is not valid!" % invoke)))
+
+
+client.run(TOKEN)
