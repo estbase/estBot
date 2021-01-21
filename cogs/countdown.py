15dc0869e9440056c54e87812b80ae5c4da174be
oriol <onavascuez@gmail.com>
2021-01-21 23:23:32 +0100

    Fixed countdown timer cog

--- cogs/countdown.py
+++ cogs/countdown.py
@@ -8,17 +8,25 @@ class Countdown(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, description="Show a countdown with the title provided. Ex.: countdown 10 My timer")
     async def countdown(self, ctx, seconds, *, title):
-        counter = 0
+        '''Countdown timer
+
+        Parameters
+        ------------
+        time: numeric
+            Seconds of the countdown
+        title: text
+            Indicates the title of your countdown
+        '''
         try:
             secondint = int(seconds)
             if secondint > 300:
                 await ctx.send("I don't think I am allowed to do go above 300 seconds \U0001f914")
-                raise BaseException
+                return
             if secondint < 0 or secondint == 0:
                 await ctx.send("I don't think I am allowed to do negatives \U0001f914")
-                raise BaseException
+                return
             message = await ctx.send("```css" + "\n" + "[" + title + "]" + "\nTimer: " + seconds + "```")
             while True:
                 secondint = secondint - 1
@@ -27,7 +35,7 @@ class Countdown(commands.Cog):
                     break
                 await message.edit(content=("```css" + "\n" + "[" + title + "]" + "\nTimer: {0}```".format(secondint)))
                 await asyncio.sleep(1)
-            await self.bot.send_message(ctx.message.channel, ctx.message.author.mention + " Your countdown " + "[" + title + "]"  + " Has ended!")
+            await ctx.send(ctx.message.author.mention + " Your countdown " + "[" + title + "]" + " has ended!")
         except ValueError:
             await ctx.send("Must be a number!")
 
